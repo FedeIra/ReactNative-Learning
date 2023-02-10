378492cdcd17d94b6be0045ecd6c62f9bcb2e7ab
Federico Irarrazaval <fedeirar@gmail.com>
2023-02-10 19:14:33 -0300

    basics 2

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+/* import { StatusBar } from 'expo-status-bar'; */
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log('hola');
   return (
     <View style={styles.container}>
       <Text>Hola soy federico!</Text>
-
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
