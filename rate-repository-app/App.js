ed40e30b286466249ca82966e4fcb0742c6c24b2
Federico Irarrazaval <fedeirar@gmail.com>
2023-02-11 00:25:33 -0300

    added component main

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 /* import { StatusBar } from 'expo-status-bar'; */
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  Alert,
+} from 'react-native';
+import Main from './src/components/Main.jsx';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola soy federico!</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
-  );
+  return <Main />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
